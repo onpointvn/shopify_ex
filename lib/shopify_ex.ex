787a870d05838afd63a67befcb68f30d72faad0d
Khanh Nguyen <nguyenkhanh@onpoint.vn>
2022-02-22 10:07:28 +0700

    Initialize project

---
+++ lib/shopify_ex.ex
@@ -0,0 +1,18 @@
+defmodule ShopifyEx do
+  @moduledoc """
+  Documentation for `ShopifyEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ShopifyEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/shopify_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule ShopifyExTest do
+  use ExUnit.Case
+  doctest ShopifyEx
+
+  test "greets the world" do
+    assert ShopifyEx.hello() == :world
+  end
+end
