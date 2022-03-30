c4cb6a185c7010347c13a5c8fffddba68021ed2a
Khanh Nguyen <nguyenkhanh@onpoint.vn>
2022-03-30 14:53:29 +0700

    Add API for updating inventory

--- lib/domains/inventory.ex
+++ lib/domains/inventory.ex
@@ -18,10 +18,35 @@ defmodule ShopifyEx.Inventory do
 
   **Reference**
 
-   https://shopify.dev/api/admin-rest/2022-01/resources/inventorylevel#post-inventory-levels-adjust
+  https://shopify.dev/api/admin-rest/2022-01/resources/inventorylevel#post-inventory-levels-adjust
   """
   @spec adjust_inventory_level(Client.t(), map()) :: {:ok, map()} | {:error, binary() | map()}
   defdelegate adjust_inventory_level(client, params),
     to: ShopifyEx.Inventory.AdjustInventoryLevelAction,
+    as: :perform
+
+  @doc """
+  Sets the inventory level for an inventory item at a location
+
+  **Reference**
+
+  https://shopify.dev/api/admin-rest/2022-01/resources/inventorylevel#post-inventory-levels-set
+  """
+  @spec set_inventory_level(Client.t(), map()) :: {:ok, map()} | {:error, binary() | map()}
+  defdelegate set_inventory_level(client, params),
+    to: ShopifyEx.Inventory.SetInventoryLevelAction,
+    as: :perform
+
+  @moduledoc """
+  Retrieves a list of inventory levels
+
+  **Reference**
+
+  https://shopify.dev/api/admin-rest/2022-01/resources/inventorylevel#get-inventory-levels
+  """
+  @spec retrieve_inventory_levels(Client.t(), map()) ::
+          {:ok, %{inventory_levels: list(map()), page_info: map()}} | {:error, binary() | map()}
+  defdelegate retrieve_inventory_levels(client, params \\ %{}),
+    to: ShopifyEx.Inventory.RetrieveInventoryLevelsAction,
     as: :perform
 end
